d63b50c3b7c96f95566490d235f5ff96470542ec
baglia <baglia3@gmail.com>
2021-02-16 19:55:05 +0100

    1

--- Main.Designer.vb
+++ Main.Designer.vb
@@ -22,18 +22,105 @@ Partial Class Main
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.grpSimulazione = New System.Windows.Forms.GroupBox()
+        Me.panelSimTop = New System.Windows.Forms.Panel()
+        Me.lblP_partenza = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSetPosArrivo = New System.Windows.Forms.Button()
+        Me.btnSetPosPartenza = New System.Windows.Forms.Button()
+        Me.lblP_arrivo = New System.Windows.Forms.Label()
+        Me.grpSimulazione.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'grpSimulazione
+        '
+        Me.grpSimulazione.Controls.Add(Me.panelSimTop)
+        Me.grpSimulazione.Location = New System.Drawing.Point(12, 12)
+        Me.grpSimulazione.Name = "grpSimulazione"
+        Me.grpSimulazione.Size = New System.Drawing.Size(449, 426)
+        Me.grpSimulazione.TabIndex = 0
+        Me.grpSimulazione.TabStop = False
+        Me.grpSimulazione.Text = "Simulazione"
+        '
+        'panelSimTop
+        '
+        Me.panelSimTop.Location = New System.Drawing.Point(6, 21)
+        Me.panelSimTop.Name = "panelSimTop"
+        Me.panelSimTop.Size = New System.Drawing.Size(437, 399)
+        Me.panelSimTop.TabIndex = 0
+        '
+        'lblP_partenza
+        '
+        Me.lblP_partenza.AutoSize = True
+        Me.lblP_partenza.Location = New System.Drawing.Point(6, 29)
+        Me.lblP_partenza.Name = "lblP_partenza"
+        Me.lblP_partenza.Size = New System.Drawing.Size(126, 17)
+        Me.lblP_partenza.TabIndex = 1
+        Me.lblP_partenza.Text = "(X,Y) PARTENZA: "
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnSetPosArrivo)
+        Me.GroupBox1.Controls.Add(Me.btnSetPosPartenza)
+        Me.GroupBox1.Controls.Add(Me.lblP_arrivo)
+        Me.GroupBox1.Controls.Add(Me.lblP_partenza)
+        Me.GroupBox1.Location = New System.Drawing.Point(467, 33)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(321, 100)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'btnSetPosArrivo
+        '
+        Me.btnSetPosArrivo.Location = New System.Drawing.Point(240, 53)
+        Me.btnSetPosArrivo.Name = "btnSetPosArrivo"
+        Me.btnSetPosArrivo.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetPosArrivo.TabIndex = 4
+        Me.btnSetPosArrivo.Text = "SET"
+        Me.btnSetPosArrivo.UseVisualStyleBackColor = True
+        '
+        'btnSetPosPartenza
+        '
+        Me.btnSetPosPartenza.Location = New System.Drawing.Point(240, 24)
+        Me.btnSetPosPartenza.Name = "btnSetPosPartenza"
+        Me.btnSetPosPartenza.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetPosPartenza.TabIndex = 3
+        Me.btnSetPosPartenza.Text = "SET"
+        Me.btnSetPosPartenza.UseVisualStyleBackColor = True
+        '
+        'lblP_arrivo
+        '
+        Me.lblP_arrivo.AutoSize = True
+        Me.lblP_arrivo.Location = New System.Drawing.Point(6, 59)
+        Me.lblP_arrivo.Name = "lblP_arrivo"
+        Me.lblP_arrivo.Size = New System.Drawing.Size(104, 17)
+        Me.lblP_arrivo.TabIndex = 2
+        Me.lblP_arrivo.Text = "(X,Y) ARRIVO: "
+        '
+        'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grpSimulazione)
         Me.MaximizeBox = False
-        Me.Name = "Form1"
+        Me.Name = "Main"
         Me.Text = "Form1"
+        Me.grpSimulazione.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents grpSimulazione As GroupBox
+    Friend WithEvents panelSimTop As Panel
+    Friend WithEvents lblP_partenza As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSetPosArrivo As Button
+    Friend WithEvents btnSetPosPartenza As Button
+    Friend WithEvents lblP_arrivo As Label
 End Class
--- Main.vb
+++ Main.vb
@@ -1,5 +1,46 @@
 ﻿Public Class Main
+
+    Dim pointPartenza As New Point
+    Dim pointArrivo As New Point
+    Dim setPartenza As Boolean = False
+    Dim setArrivo As Boolean = False
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        lblP_partenza.Text = lblP_partenza.Text + " (0;0)"
+        lblP_arrivo.Text = lblP_arrivo.Text + " (0;0)"
+
+    End Sub
+
+    Private Sub panelSimTop_MouseDown(sender As Object, e As MouseEventArgs) Handles panelSimTop.MouseDown 'funzione viene lanciata 
+        'quando clicco sul form (evento mouseDown)
+
+        If setPartenza Then 'se sto settando la partenza salvo le coordinate del punto in cui ho cliccato in pointPartenza
+            pointPartenza.X = e.X
+            pointPartenza.Y = e.Y
+            setPartenza = False
+            lblP_partenza.Text = "(X,Y) PARTENZA: (" + CStr(e.X) + "," + CStr(e.Y) + ")"
+        End If
+
+        If setArrivo Then
+            pointArrivo.X = e.X
+            pointArrivo.Y = e.Y
+            setArrivo = False
+            lblP_arrivo.Text = "(X,Y) PARTENZA: (" + CStr(e.X) + "," + CStr(e.Y) + ")"
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnSetPosPartenza_Click(sender As Object, e As EventArgs) Handles btnSetPosPartenza.Click
+        If setArrivo = False Then 'verifico che non sia già attivo set arrivo
+            setPartenza = True
+        End If
+    End Sub
+
+    Private Sub btnSetPosArrivo_Click(sender As Object, e As EventArgs) Handles btnSetPosArrivo.Click
+        If setPartenza = False Then 'verifico che non sia già attivo set partenza
+            setArrivo = True
+        End If
     End Sub
 End Class
